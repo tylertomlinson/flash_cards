e392918d03f4feefe9b9ab0c7af73e0229d6f991
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-02 20:05:40 -0700

    Added better design to output.

--- lib/round.rb
+++ lib/round.rb
@@ -30,8 +30,8 @@ class Round
   end
 
   def percent_correct_by_category(category)
-    total_in_category = turns.count { |turn| turn.card.category == category}
-    100 * number_correct_by_category(category).to_f / total_in_category
+    total_in_category = @turns.count { |turn| turn.card.category == category}
+    (number_correct_by_category(category).to_f / total_in_category) * 100
   end
 
   def current_card_index
@@ -48,25 +48,27 @@ class Round
 
   def start
     puts "Welcome! You are playing with #{size_of_deck} cards." + "\n\n"
-    puts "-------------GOOD LUCK----------------" + "\n\n"
+    puts "-=-=-=-=-=-=-GOOD LUCK-=-=-=-=-=-=-" + "\n\n"
 
     until current_card == nil
-    puts "This is card number #{current_card_index} out of #{size_of_deck}"
-    puts current_card.question
-    #need to have format of user response have correct formatting when comparing to answer.
-    user_guess = gets.chomp.to_s
+    puts "--------------------------------------" + "\n"
+    puts "This is card number #{current_card_index} out of #{size_of_deck}" + "\n"
+    puts "\n" + current_card.question
+
+    user_guess = gets.chomp
     take_turn(user_guess)
     puts "Your answer is: #{answer_feedback}"
     end
-  
-    puts "------------- Game over! -------------" + "\n\n"
+
+    puts "\n\n" "-=-=-=-=-=-=- Game over! -=-=-=-=-=-=-" + "\n\n"
+    puts "Press ENTER to see results"
+    user_input = gets.chomp
 
     puts "You had #{number_correct} correct answers out of #{size_of_deck} for a total score of #{percent_correct.to_i}%"
 
-    puts "History - #{percent_correct_by_category(:History)}% correct"
-    puts "Sports - #{percent_correct_by_category(:Sports)}% correct"
-    puts "Entertainment - #{percent_correct_by_category(:Entertainment
-    )}% correct"
+    puts "American History - #{percent_correct_by_category("American History")}% correct"
+    puts "World History - #{percent_correct_by_category("World History")}% correct"
+    puts "Sports - #{percent_correct_by_category("Sports")}% correct"
+    puts "Entertainment - #{percent_correct_by_category("Entertainment")}% correct"
   end
-
 end
