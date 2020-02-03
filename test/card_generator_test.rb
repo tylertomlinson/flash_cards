5b5b8812e1348d8d5043c730c745b19f8a45326c
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-03 12:11:35 -0700

    Test for question,answer,category for card.

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -15,4 +15,11 @@ class CardGeneratorTest < Minitest::Test
   def test_it_creates_cards
     assert_equal 10, @cardgenerator.cards.count
   end
+
+  def test_cards_have_questions_answers_and_category
+    skip
+    assert_equal "The citadel Machu Picchu is located in which Latin American Country?", @card_generator.card.question
+    assert_equal "Peru", @CardGenerator.card.answer
+    assert_equal "World Hisotry", @card_generator.card.category
+  end
 end
