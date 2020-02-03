b507b17d07638d1e8a3cc7d20d650115bb129790
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-02 19:32:56 -0700

    Added card_info method.

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,5 +1,14 @@
 require "./lib/card"
 
 class CardGenerator
-  
+
+  attr_reader :filename
+
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def card_info
+    (File.read(@filename).split("\n")).collect {|card| card.split(",")}
+  end
 end
