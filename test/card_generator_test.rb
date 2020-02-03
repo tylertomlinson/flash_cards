9c23c9e9a279e478f50bf6c3b29ffd2a48a00322
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-02 19:31:33 -0700

    Added test for CardGenerator class.

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -5,4 +5,15 @@ require './lib/card_generator'
 
 class CardGeneratorTest < Minitest::Test
 
+  def setup
+    @cardgenerator = CardGenerator.new('cards.txt')
+  end
+
+  def test_it_exists
+    assert_instance_of CardGenerator, @cardgenerator
+  end
+
+  def test_it_creates_cards
+    assert_equal 3, @cardgenerator.cards.count
+  end
 end
